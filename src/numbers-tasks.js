44be0c311b9749bbfbf2c0ac7dfaf8b8d36a0ae0
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-11-13 16:30:45 -0500

    feat: add solution for isSafeInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -469,8 +469,8 @@ function getIntegerOnString(str, base) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(num) {
+  return Number.isSafeInteger(num);
 }
 
 /**
